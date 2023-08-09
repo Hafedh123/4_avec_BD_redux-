f24811ba0531f1f457421e8924cacd9ad36053fd
Tarek Bchini <tarek.bchini83@gmail.com>
2023-08-09 03:58:26 +0200

    version_basique

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,35 @@
+import { Row, Col } from 'react-bootstrap'
+//import products from '../products'
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
+import Product from '../components/Product'
+
+const HomeScreen = () => {
+
+  //importer les produits depuis le backend
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+   const fetchProducts = async () => {
+    //le chemin le prend depuis proxy dans package.json de frontend
+    const { data } = await axios.get('/api/products');
+    setProducts(data);
+  };
+  fetchProducts();
+  }, []);
+ 
+  return (
+    <>
+        <h1>Latest Products</h1>
+        <Row>
+           {products.map((product) => (
+               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                 <Product product={product} />
+               </Col>
+           ))}
+        </Row>
+    </>
+  )
+}
+
+export default HomeScreen
